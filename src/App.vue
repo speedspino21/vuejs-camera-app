3e5c27e811a3c986e3e7a3ef96ecf78e9d9b0965
Yanming Zhang <yzhan27@outlook.com>
2020-10-13 06:00:46 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png">
+		<video autoplay></video>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'app',
+	components: {},
+	data: function () {
+		return {
+			stream: null
+		}
+	},
+	methods: {
+		init: async function () {
+			// if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+			// 	let constraints = {
+			// 		video: {
+			// 			width: { min: 1280 },
+			// 			height: { min: 720 }
+			// 		},
+			// 	};
+
+			// 	const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+			// 	const video = document.querySelector('video');
+			// }
+		},
+		getDevices: async function () {
+			// const devices = await navigator.mediaDevices.enumerateDevices();
+		}
+	},
+	beforeMount: function () {
+		this.init();
+	}
+}
+</script>
+
+<style>
+video {
+	width: 100%;
+	background: rgba(0, 0, 0, 0.2);
+}
+</style>
